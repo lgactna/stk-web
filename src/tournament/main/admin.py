13685f92645842c9a76a55fd09e87429cc851d7d
lgactna <48928674+lgactna@users.noreply.github.com>
2020-12-15 21:34:11 -0800

    admin model registrations

--- src/tournament/main/admin.py
+++ src/tournament/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Player, Role, Team, Score, Map, Mappool, Match, Stage
+
 # Register your models here.
+
+admin.site.register(Player)
+admin.site.register(Role)
+admin.site.register(Team)
+admin.site.register(Score)
+admin.site.register(Map)
+admin.site.register(Mappool)
+admin.site.register(Match)
+admin.site.register(Stage)
