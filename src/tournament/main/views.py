eda652054639be82184e4693580ee51635ff49f3
lgactna <48928674+lgactna@users.noreply.github.com>
2020-12-16 23:11:53 -0800

    very basic player detail page

--- src/tournament/main/views.py
+++ src/tournament/main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from django.views import generic
+
+from main.models import Player, Team, Map, Mappool
+
 # Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_players = Player.objects.all().count()
+
+    context = {
+        'num_players': num_players,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+class PlayerDetailView(generic.DetailView):
+    model = Player
